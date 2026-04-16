3e51ea4f472d7ae11c062a3c10d4fcb963b30f48
inumaruh <inumaruh@C010422856.zipangu.cac.co.jp>
2026-04-16 14:46:09 +0900

    basic

--- java_comprehension_exercises_volume1/src/basic/question06/Question06_4.java
+++ java_comprehension_exercises_volume1/src/basic/question06/Question06_4.java
@@ -1,12 +1,30 @@
 package basic.question06;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Question06_4 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-		// TODO:ここに実装
+		int amountBooks = 0;
+		int sellFlag = 0;
+		int bookprice = 200;
+		System.out.println("本を売却します。");
+
+		do {
+			System.out.println("売却する本の冊数を１つ増やしますか？");
+			System.out.println("はい：０，いいえ：１>");
+			String str = reader.readLine();
+			sellFlag = Integer.parseInt(str);
+			amountBooks++;
+
+		} while (sellFlag == 0);
+
+		System.out.println("売却する本の冊数は" + (amountBooks - 1) + "冊です。");
+		System.out.println("売却した本の合計金額は" + (amountBooks - 1) * bookprice + "円です。");
 
 	}
 
